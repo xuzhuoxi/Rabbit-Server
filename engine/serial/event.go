fd8ec424e1b2049e749be489da967a505a3f7e72
xuzhuoxi <xuzhuoxi@gmail.com>
2023-08-01 14:33:23 +0800

    1. 增加队列串行处理功能。

---
+++ engine/serial/event.go
@@ -0,0 +1,12 @@
+// Package serial
+// Create on 2023/7/2
+// @author xuzhuoxi
+package serial
+
+const (
+	EventOnModuleStarted = "Observer:EventOnObserverStarted"
+	EventOnModuleStopped = "Observer:EventOnObserverStopped"
+
+	EventOnManagerStarted = "Observer-Manager:EventOnManagerStarted"
+	EventOnManagerStopped = "Observer-Manager:EventOnManagerStopped"
+)
