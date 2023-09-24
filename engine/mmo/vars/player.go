ebf992c9af95599e5173afdb83f22591bb068ae4
xuzhuoxi <xuzhuoxi@gmail.com>
2023-09-24 17:13:39 +0800

    1. mmo: update var keys.

--- engine/mmo/vars/player.go
+++ engine/mmo/vars/player.go
@@ -1,27 +1,31 @@
-// Package vars
-// Create on 2023/9/3
-// @author xuzhuoxi
 package vars
-
 const (
-	// PlayerPosX X位置(int32)
-	PlayerPosX string = "x"
-	// PlayerPosY Y位置(int32)
-	PlayerPosY string = "y"
-	// PlayerPosZ Z位置(int32)
-	PlayerPosZ string = "z"
-	// PlayerDirection 面向(uint8)(8方向)
-	PlayerDirection string = "d"
-	// PlayerAction 动作(uint32)
-	PlayerAction string = "a"
-	// PlayerHp 耐久(uint32)
-	PlayerHp string = "hp"
-	// PlayerBuff Buff(uint32), 每一个位代表一个buff
-	PlayerBuff string = "pbf"
-	// PlayerNick 昵称(string)
-	PlayerNick string = "pn"
-	// PlayerTeam 队伍id(string)
-	PlayerTeam string = "pt"
-	// PlayerTeamCorps 军团Id(string)
-	PlayerTeamCorps string = "pc"
+    // PlayerPosX X位置(int32)
+    PlayerPosX string = "x"
+    // PlayerPosY Y位置(int32)
+    PlayerPosY string = "y"
+    // PlayerPosZ Z位置(int32)
+    PlayerPosZ string = "z"
+    // PlayerInputX 输入状态X(int32)
+    PlayerInputX string = "ix"
+    // PlayerInputY 输入状态Y(int32)
+    PlayerInputY string = "iy"
+    // PlayerInputZ 输入状态Z(int32)
+    PlayerInputZ string = "iz"
+    // PlayerInputJump 输入状态Jump(bool)
+    PlayerInputJump string = "ij"
+    // PlayerFace 面向(uint8)(8方向)
+    PlayerFace string = "f"
+    // PlayerAction 动作(uint32)
+    PlayerAction string = "a"
+    // PlayerHp 耐久(uint32)
+    PlayerHp string = "hp"
+    // PlayerBuff Buff(uint32), 每一个位代表一个buff
+    PlayerBuff string = "pbf"
+    // PlayerNick 昵称(string)
+    PlayerNick string = "pn"
+    // PlayerTeam 队伍id(string)
+    PlayerTeam string = "pt"
+    // PlayerTeamCorps 军团Id(string)
+    PlayerTeamCorps string = "pc"
 )
--- engine/mmo/vars/room.go
+++ engine/mmo/vars/room.go
@@ -1,9 +1,5 @@
-// Package vars
-// Create on 2023/9/3
-// @author xuzhuoxi
 package vars
-
 const (
-	// RoomName 房间名称(string)
-	RoomName string = "rn"
+    // RoomName 房间名称(string)
+    RoomName string = "rn"
 )
